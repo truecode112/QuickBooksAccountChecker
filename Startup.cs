5b39a9a9c87b6a316fc89c155dc125995d4b783d
truecode112 <truecoder112@gmail.com>
2023-02-13 04:24:26 +0900

    Added few files

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using Owin;
+using log4net;
+
+[assembly: OwinStartup(typeof(QBOAccountChecker.Startup))]
+
+namespace QBOAccountChecker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+                    AuthenticationType = "Cookies"
+                });
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "TempState",
+                AuthenticationMode = AuthenticationMode.Passive
+            });
+
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+
+    }
+}
